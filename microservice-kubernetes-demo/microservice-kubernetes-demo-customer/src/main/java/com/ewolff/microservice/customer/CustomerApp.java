a47dd677587180f6ddb939b7eaca5424bc847df5
EC2 Default User <ec2-user@ip-172-31-35-86.us-east-2.compute.internal>
2020-06-23 21:23:28 +0000

    Updated the code

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/CustomerApp.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/CustomerApp.java
@@ -24,7 +24,7 @@ public class CustomerApp {
 	public void generateTestData() {
 		customerRepository
 				.save(new Customer("Eberhard", "Wolff", "eberhard.wolff@gmail.com", "Unter den Linden", "Berlin"));
-		customerRepository.save(new Customer("Rod", "Johnson", "rod@somewhere.com", "Market Street", "San Francisco"));
+		customerRepository.save(new Customer("Rod", "Joseph", "rod@somewhere.com", "Market Street", "San Francisco"));
 	}
 
 	public static void main(String[] args) {
