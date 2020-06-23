33b33e38679d13d1d260285b192a14f060d8922d
EC2 Default User <ec2-user@ip-172-31-44-35.us-east-2.compute.internal>
2020-06-23 13:21:41 +0000

    Updated with Docker Image build

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,14 +1,30 @@
 pipeline {
-    agent any
-    stages {
-        stage('Build') {
-		steps {
-                	sh script:'''
-          		#!/bin/bash
-          		cd ~/workspace/Test-maven/microservice-kubernetes-demo
-          		./mvnw -B -DskipTests clean package
-        		'''
-            }	 
+	agent any
+	environment{
+		VERSION_TAG = getVersionTag()
+	}
+	stages {
+		stage('Code Build') {
+			steps {
+				sh script:'''
+				#!/bin/bash
+				cd ~/workspace/Test-maven/microservice-kubernetes-demo
+				./mvnw -B -DskipTests clean package
+				'''
+			}
+		}
+		stage ('Building Docker Image') {
+			steps {
+				withCredentials([usernamePassword(credentialsId: 'vaishnavinaren219', passwordVariable: 'DOCKER_PWD', usernameVariable: 'DOCKER_ACCOUNT')]) {
+					sh "docker login -u $DOCKER_ACCOUNT -p $DOCKER_PWD"
+					sh "./docker-build.sh"
+					
+			}
+				
         }
     }
+}
+
+def getVersionTag(){
+	def tag = sh script: 'git rev-parse HEAD', returnStdout: true
 }
--- microservice-kubernetes-demo/docker-build.sh
+++ microservice-kubernetes-demo/docker-build.sh
@@ -2,18 +2,19 @@
 if [ -z "$DOCKER_ACCOUNT" ]; then
     DOCKER_ACCOUNT=ewolff
 fi;
+echo "Building image under $DOCKER_ACCOUNT"
 docker build --tag=microservice-kubernetes-demo-apache apache
-docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest
+docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:$VERSION_TAG
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache
 
 docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
-docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest
+docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:$VERSION_TAG
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog
 
 docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
-docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest
+docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:$VERSION_TAG
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer
 
 docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
-docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest
+docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:$VERSION_TAG
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order
