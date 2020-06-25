c590f0f7e0ac4c9f8c75b5b9e48dcc868d040bb6
EC2 Default User <ec2-user@ip-172-31-35-86.us-east-2.compute.internal>
2020-06-25 14:47:37 +0000

    Updated port

--- microservice-kubernetes-demo/kubernetes-deploy.sh
+++ microservice-kubernetes-demo/kubernetes-deploy.sh
@@ -4,9 +4,9 @@ if [ -z "$DOCKER_ACCOUNT" ]; then
 fi;
 kubectl run apache --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest --port=80
 kubectl expose deployment/apache --type="LoadBalancer" --port 80
-kubectl run catalog --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest --port=8080
-kubectl expose deployment/catalog --type="LoadBalancer" --port 8080
-kubectl run customer --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest --port=8080
-kubectl expose deployment/customer --type="LoadBalancer" --port 8080
-kubectl run order --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest --port=8080
-kubectl expose deployment/order --type="LoadBalancer" --port 8080
+kubectl run catalog --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest --port=8081
+kubectl expose deployment/catalog --type="LoadBalancer" --port 8081
+kubectl run customer --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest --port=8081
+kubectl expose deployment/customer --type="LoadBalancer" --port 8081
+kubectl run order --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest --port=8081
+kubectl expose deployment/order --type="LoadBalancer" --port 8081
